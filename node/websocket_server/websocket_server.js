c978f7030eaba28d4a96b2777ee531b095865ffb
Erik Young <erik_young@mac.com>
2022-03-17 09:30:28 -0400

    websocket_experiments

--- node/websocket_server/websocket_server.js
+++ node/websocket_server/websocket_server.js
@@ -1,43 +1,124 @@
 'use strict';
 
-const dns = require('dns');
-const dnsPromises = dns.promises;
-const options = {
-    family: 4,
-    hints: dns.ADDRCONFIG | dns.V4MAPPED
+// Import Ceramic HTTP client so that we can connect to a Ceramic node.
+import { CeramicClient } from '@ceramicnetwork/http-client';
+ 
+import { randomBytes } from 'crypto'
+
+// Import 3ID DID libraries so that we can create, resolve, and use 3ID DID accounts.
+import { DID } from 'dids';
+import { getResolver as getKeyResolver } from 'key-did-resolver'
+import { getResolver as get3IDResolver} from '@ceramicnetwork/3id-did-resolver'
+import ThreeIdProvider from '3id-did-provider'
+
+// We connect to the clay test network.
+const API_URL = 'https://ceramic-clay.3boxlabs.com'
+const ceramic = new CeramicClient(API_URL)
+
+// DID Functions
+
+// 3ID DID Provider
+
+// We need an auth secret. This is 32 bytes of entropy that we use to authenticate.
+const seed = randomBytes(32)
+const authId = 'myAuthID'
+
+async function authenticateWithSecret(seed) {
+  const threeID = await ThreeIdProvider.create({
+    authSecret,
+    getPermission: ( request ) => Promise.resolve(request.payload.paths),
+  })
+
+  const did = new DID({
+    provider: threeID.getDidProvider(),
+    resolver: {
+      ...get3IDResolver(ceramic),
+      ...getKeyResolver(),
+    },
+  })
+
+  // We use the 3ID Provider to authenticate the DID.
+  await did.authenticate()
+  
+  ceramic.did = did
 }
 
-options.all = true;
+console.log(ceramic);
 
-// The following block works, but it's a little vebose.
-const { networkInterfaces } = require('os');
-const networks = networkInterfaces();
-console.log(networks);
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(networks)) {
-    for (const network of networks[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        if (network.family === 'IPv4' && !network.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(network.address);
-        }
-    }
+// 3ID DID Resolver
+function createCeramicWith3ID() {
+  const did = new DID({
+    resolver: {
+      ...get3IDResolver(ceramic),
+      ...getKeyResolver(),
+    },
+  })
 }
 
-console.log(results)
+async function load(id) {
+  await ceramic.loadStream(id);
+}
+
+// import { DID } from 'dids'
+// import KeyDidResolver from 'key-did-resolver'
+// import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+import os from 'os'
+import http from 'http'
+
+// Create a ThreeIdConnect connect instance as soon as possible in your app to start loading assets
+// const threeID = new ThreeIdConnect()
+
+async function authenticateWithEthereum(ethereumProvider) {
+    // Request accounts from the Ethereum provider
+    const accounts = await ethereumProvider.request({
+      method: 'eth_requestAccounts',
+    })
+    // Create an EthereumAuthProvider using the Ethereum provider and requested account
+    const authProvider = new EthereumAuthProvider(ethereumProvider, accounts[0])
+    // Connect the created EthereumAuthProvider to the 3ID Connect instance so it can be used to
+    // generate the authentication secret
+    await threeID.connect(authProvider)
+  
+    const ceramic = new CeramicClient()
+    const did = new DID({
+      // Get the DID provider from the 3ID Connect instance
+      provider: threeID.getDidProvider(),
+      resolver: {
+        ...get3IDResolver(ceramic),
+        ...getKeyResolver(),
+      },
+    })
+    ceramic.did = did
+}
+
+// Enumerate assigned IP addresses.
+const { networkInterfaces } = os
+// const networks = networkInterfaces();
+// console.log(networks);
+// const results = Object.create(null); // Or just '{}', an empty object
+
+// for (const name of Object.keys(networks)) {
+//     for (const network of networks[name]) { 
+//         // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses.
+//         if (network.family === 'IPv4' && !network.internal) {
+//             if (!results[name]) {
+//                 results[name] = [];
+//             }
+//             results[name].push(network.address);
+//         }
+//     }
+// }
+
 // console.log(results)
 
 // The following works as a simple server for testing.
 
-// const request = function (req, res) {
-//     res.writeHead(200);
-//     res.end('Hello');
-// }
+const request = function (req, res) {
+    res.writeHead(200);
+    res.end('Hello');
+}
 
-// const server = http.createServer(request);
-// console.log("websocket_server server created.")
-// console.log("websocket_server listening on port 3000...")
-// server.listen(3000);
+const server = http.createServer(request);
+console.log("websocket_server server created.")
+console.log("websocket_server listening on port 3000...")
+server.listen(3000);

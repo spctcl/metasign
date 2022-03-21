05430695f204f2fe196d8204e116c93b7c802932
Erik Young <erik_young@mac.com>
2022-03-21 09:59:42 -0400

    Added auth.

--- data_model_scripts/createModels.js
+++ data_model_scripts/createModels.js
@@ -1,6 +1,9 @@
 import { readFile, writeFile } from 'node:fs/promises'
 import { CeramicClient } from '@ceramicnetwork/http-client'
 import { ModelManager } from '@glazed/devtools'
+import { DID } from 'dids'
+import { getResolver as getKeyResolver } from 'key-did-resolver'
+import { getResolver as get3IDResolver } from '@ceramicnetwork/3id-did-resolver'
 
 // The encoded model could be imported from the file system for example
 const bytes = await readFile(new URL('deviceSchema.json', import.meta.url))
@@ -9,6 +12,17 @@ console.log(encodedModel)
 
 const ceramic = new CeramicClient("http://localhost:7007")
 console.log("ceramic: ", ceramic)
+
+const did = new DID({
+  resolver: {
+	// A Ceramic client instance is needed by the 3ID DID resolver to load DID documents
+	...get3IDResolver(ceramic),
+	// `did:key` DIDs are used internally by 3ID DIDs, therefore the DID instance must be able to resolve them
+	...getKeyResolver(),
+  },
+})
+// This will allow the Ceramic client instance to resolve DIDs using the `did:3` method
+ceramic.did = did
 
 const manager = new ModelManager(ceramic)
 
@@ -41,10 +55,10 @@ await manager.createSchema('deviceSchema', {
 	},
 	required: ['deviceName'],
 	})
-// const manager = ModelManager.fromJSON(ceramic, encodedModel)
+//const manager = ModelManager.fromJSON(ceramic, encodedModel)
 
 // // The published model could then itself be exported to be used at runtime
 // const publishedModel = await manager.toPublished()
+
+// console.log("published", publishedModel);
 // await writeFile(new URL('published-model.json', import.meta.url), JSON.stringify(publishedModel))
-
-
